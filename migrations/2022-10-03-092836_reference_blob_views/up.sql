1a178d64f42bc4298bc0926d67c9b27fd3e0829f
Till <31419678+tifrel@users.noreply.github.com>
2022-10-03 14:16:49 +0200

    Adding `reference_blob` to all applicable views (#8)
    * Adding reference_blob to all applicable views
    
    * Adding hasura permissions

---
+++ migrations/2022-10-03-092836_reference_blob_views/up.sql
@@ -0,0 +1,62 @@
+drop view mb_views.nft_activities;
+drop view mb_views.nft_metadata_unburned;
+drop view mb_views.nft_tokens_with_listing;
+
+create view mb_views.nft_metadata_unburned as
+select
+	m.id as metadata_id,
+	m.title,
+	m.description,
+	m.media,
+  m.reference_blob,
+	t.nft_contract_id,
+	t.minted_timestamp,
+  t.minter,
+	l.price
+from nft_metadata m
+left join (
+  select distinct on (metadata_id) *
+  from nft_tokens
+  order by metadata_id, minted_timestamp, burned_timestamp
+) t on m.id=t.metadata_id
+left join mb_views.active_listings_rollup l on m.id=l.metadata_id
+where t.burned_timestamp is null;
+
+create view mb_views.nft_tokens_with_listing as
+select
+	t.nft_contract_id,
+	t.token_id,
+	t.owner,
+	t.metadata_id,
+  l.reference_blob,
+	l.price
+from nft_tokens t
+left join mb_views.active_listings l on l.nft_contract_id=t.nft_contract_id
+where t.burned_timestamp is null;
+
+create view mb_views.nft_activities as
+select
+  a.receipt_id,
+  a.tx_sender,
+  a.timestamp,
+  a.nft_contract_id,
+  a.token_id,
+  a.kind,
+  a.action_sender,
+  a.action_receiver,
+  a.price,
+  t.reference,
+  t.reference_hash,
+  t.copies,
+  m.title,
+  m.description,
+  m.media,
+  m.media_hash,
+  m.extra,
+  m.reference_blob
+from nft_activities a
+  left join nft_tokens t
+    on a.nft_contract_id = t.nft_contract_id
+    and a.token_id = t.token_id
+  left join nft_metadata m
+    on t.metadata_id = m.id;
