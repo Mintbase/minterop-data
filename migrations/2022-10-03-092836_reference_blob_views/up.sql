b3cedc0733829a17d536bf735a6fae43e3bf21c8
Till <till.friesewinkel@googlemail.com>
2022-10-03 11:17:09 +0100

    `mb_views` prefix

--- migrations/2022-10-03-092836_reference_blob_views/up.sql
+++ migrations/2022-10-03-092836_reference_blob_views/up.sql
@@ -1,6 +1,6 @@
-drop view nft_activities;
-drop view nft_metadata_unburned;
-drop view nft_tokens_with_listing;
+drop view mb_views.nft_activities;
+drop view mb_views.nft_metadata_unburned;
+drop view mb_views.nft_tokens_with_listing;
 
 create view mb_views.nft_metadata_unburned as
 select
