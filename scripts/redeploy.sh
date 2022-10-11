cd25a8bf3a348c92e3e461b8645835d0554b9f16
Till <till.friesewinkel@googlemail.com>
2022-10-11 13:46:02 +0100

    Trigger permissions

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -11,6 +11,12 @@ grant select on all sequences in schema mb_views to hasura;
 grant usage on schema mb_views to hasura;
 EOF
 
+read -r -d '' POSTGRES_GRANTS <<EOF
+grant usage on schema hdb_catalog to postgres;
+grant select on all tables in schema hdb_catalog to postgres;
+grant insert on all tables in schema hdb_catalog to postgres;
+EOF
+
 source "$network.env" || exit 1
 
 # run diesel migrations
@@ -23,4 +29,5 @@ DATABASE_URL="$POSTGRES" diesel migration run || exit 1
 ) || exit 1
 
 # grant privileges to hasura user
-psql "$POSTGRES" -c "$HASURA_GRANTS"
+psql "$POSTGRES" -c "$HASURA_GRANTS" || exit 1
+psql "$HASURA_POSTGRES" -c "$POSTGRES_GRANTS" || exit 1
