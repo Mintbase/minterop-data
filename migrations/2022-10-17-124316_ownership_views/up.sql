332e0848be7c409a77768fdcd897cf741c4b0ce4
cif <email@benipsen.com>
2022-10-17 14:09:33 +0100

    🗃️ Add views for owned tokens with null transfer

---
+++ migrations/2022-10-17-124316_ownership_views/up.sql
@@ -0,0 +1,6 @@
+create view mb_views.nft_owned_tokens as
+  select * from mb_views.nft_tokens
+  where burned_timestamp is null
+  order by greatest(last_transfer_timestamp, minted_timestamp) desc nulls last;
+
+create index nft_unburned_owned on nft_tokens(owner, burned_timestamp);
