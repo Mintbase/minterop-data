2300496486f0ae1ca664f9a7104b1c8cf83e8e53
Till <31419678+tifrel@users.noreply.github.com>
2022-11-02 17:03:36 +0100

    Removing token/listing multiplication (#32)

---
+++ migrations/2022-11-02-152113_fix-tokens-with-listing/down.sql
@@ -0,0 +1,12 @@
+drop view mb_views.nft_tokens_with_listing;
+create view mb_views.nft_tokens_with_listing
+as select
+	t.nft_contract_id,
+	t.token_id,
+	t.owner,
+	t.metadata_id,
+	l.price,
+  l.reference_blob
+from nft_tokens t
+left join mb_views.active_listings l on l.nft_contract_id=t.nft_contract_id
+where t.burned_timestamp is null;
