1026d153a3b0ca558f506d225adf11ead4d64f24
cif <email@benipsen.com>
2023-01-11 12:29:03 +0000

    fix: :card_file_box: Use one of two timestamps in token join

---
+++ migrations/2023-01-11-122654_use-last-transfer-in-meta/down.sql
@@ -0,0 +1,37 @@
+rop view mb_views.nft_metadata_unburned;
+
+create view mb_views.nft_metadata_unburned as
+select md.id as metadata_id,
+  md.title,
+  md.description,
+  md.media,
+  md.nft_contract_id,
+  md.base_uri,
+  md.reference_blob,
+  md.content_flag,
+  t.minted_timestamp,
+  t.minter,
+  l.price
+from nft_metadata md
+left join lateral (
+  select
+    t.minted_timestamp,
+    t.minter
+  from nft_tokens t
+  where
+    t.burned_timestamp is null and t.metadata_id = md.id
+  order by t.minted_timestamp desc
+  limit 1
+) t on true
+left join lateral (
+  select p.price
+  from nft_listings p
+  where
+    p.unlisted_at is null and
+    p.accepted_at is null and
+    p.invalidated_at is null and
+    p.metadata_id = md.id
+  order by p.price asc
+  limit 1
+) l on true
+where t.minted_timestamp is not null;
