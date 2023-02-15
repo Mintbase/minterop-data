6e4da4ef0016293f387aa252721e61227c349f59
Till <till.friesewinkel@googlemail.com>
2023-02-15 12:04:06 +0000

    Columnar format looks nicer and protects my job when Elon buys the company

--- migrations/2023-02-15-120157_attributes-search-index/up.sql
+++ migrations/2023-02-15-120157_attributes-search-index/up.sql
@@ -1,1 +1,3 @@
-create index nft_attributes_search_index on nft_attributes using gin(nft_contract_id, attribute_type, attribute_value);
+create index nft_attributes_search_index
+on nft_attributes
+using gin(nft_contract_id, attribute_type, attribute_value);
