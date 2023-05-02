ab39ed43134b1609ab35a1f85ca018227076ff66
Till <till.friesewinkel@googlemail.com>
2023-05-02 11:55:31 +0100

    Hotfixing stuffs

--- migrations/2023-05-02-094810_more-currency-fixes/up.sql
+++ migrations/2023-05-02-094810_more-currency-fixes/up.sql
@@ -110,17 +110,18 @@ create view mb_views.nft_activities_rollup as
       on a.metadata_id = m.id;
 
 drop view mb_views.nft_tokens_with_listing;
-create view mb_views.nft_tokens_with_listing
-as select
-	t.nft_contract_id,
-	t.token_id,
-	t.owner,
-	t.metadata_id,
-	l.price,
-	l.currency,
-  l.reference_blob
-from nft_tokens t
-left join mb_views.active_listings l
-  on l.nft_contract_id = t.nft_contract_id
-  and l.token_id = t.token_id
-where t.burned_timestamp is null;
+create view mb_views.nft_tokens_with_listing as
+  select
+    t.nft_contract_id,
+    t.token_id,
+    t.owner,
+    t.metadata_id,
+    l.price,
+    l.currency,
+    l.reference_blob,
+    l.content_flag
+  from nft_tokens t
+  left join mb_views.active_listings l
+    on l.nft_contract_id = t.nft_contract_id
+    and l.token_id = t.token_id
+  where t.burned_timestamp is null;
