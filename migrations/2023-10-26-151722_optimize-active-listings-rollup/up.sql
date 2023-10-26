afc8ac3644fd9fc166d66d860987452a4f1afa20
Till <31419678+tifrel@users.noreply.github.com>
2023-10-26 16:38:00 +0100

    Optimize `mb_views.active_listings_rollup` (#93)
    * Optimize `mb_views.active_listings_rollup`
    
    * Remove unnecessary ordering of metadata

---
+++ migrations/2023-10-26-151722_optimize-active-listings-rollup/up.sql
@@ -0,0 +1,35 @@
+drop view mb_views.active_listings_rollup;
+create view mb_views.active_listings_rollup as
+   select
+     l.nft_contract_id,
+     l.token_id,
+     l.market_id,
+     l.approval_id,
+     l.created_at,
+     l.receipt_id,
+     l.kind,
+     l.price,
+     l.currency,
+     l.listed_by,
+     l.metadata_id,
+     m.reference,
+     m.minter,
+     m.title,
+     m.description,
+     m.reference_blob,
+     m.media,
+     m.extra,
+     m.base_uri,
+     m.content_flag
+   from (
+     select *
+     from nft_listings
+     where unlisted_at is null
+       and accepted_at is null
+       and invalidated_at is null
+   ) l
+   left join lateral (
+     select *
+     from nft_metadata m0
+     where m0.id = l.metadata_id
+   ) m on true;
