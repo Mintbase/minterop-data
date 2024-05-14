5170ea1d6de3279ccdb1fafe8fefe55439dc8236
Till <till.friesewinkel@googlemail.com>
2024-05-14 12:18:01 +0100

    Fmt + lint

--- src/rpc_payloads.rs
+++ src/rpc_payloads.rs
@@ -1,4 +1,7 @@
-use serde::{Deserialize, Serialize};
+use serde::{
+    Deserialize,
+    Serialize,
+};
 
 type RoyaltiesMap = std::collections::HashMap<String, u16>;
 
@@ -48,7 +51,8 @@ pub enum RpcMessage {
 mod tests {
     use super::*;
     use crate::rpc_payloads::RpcMessage::{
-        HandleContractPayload, HandleTokenPayload,
+        HandleContractPayload,
+        HandleTokenPayload,
     };
     const CONTRACT_PAYLOAD_STR: &str = r#"{"kind":"contract","payload":{"contract_id":"foo.near","refresh":null}}"#;
     const TOKEN_PAYLOAD_STR: &str = r#"{"kind":"token","payload":{"contract_id":"foo.near","token_ids":["bar"],"refresh":null,"minter":"foo.near"}}"#;
